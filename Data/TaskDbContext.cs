fbf3b0f046a65b9c4aa4f0a32b92920b6e2b5f59
waclawik <kasiaw359@gmail.com>
2025-03-14 21:19:56 +0100

    Data base operations

---
+++ Data/TaskDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskManager.Models; // Dodaj referencję do modeli
+
+namespace TaskManager.Data
+{
+    public class TaskDbContext : DbContext
+    {
+        public TaskDbContext(DbContextOptions<TaskDbContext> options) : base(options) { }
+
+        public DbSet<TaskItem> Tasks { get; set; }
+    }
+}
